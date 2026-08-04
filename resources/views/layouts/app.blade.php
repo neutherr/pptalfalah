9b56f9077f1cc6e27446d7a5dd90c8158b5b882b
Muhammad Yunus Kholis <muhammad.5210411288@student.uty.ac.id>
2026-08-04 11:53:50 +0700

    style: use General Sans typography

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -68,7 +68,6 @@
     {{-- Fonts and compiled styles --}}
     <link rel="preconnect" href="https://fonts.googleapis.com"/>
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-    <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500&family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet"/>
     <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap" rel="stylesheet"/>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
